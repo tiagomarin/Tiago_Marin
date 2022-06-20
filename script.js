28fc3535086fd2ae18385c25359c2729500f7566
Tiago Marin <tiago.lelinski@gmail.com>
2022-06-20 18:03:29 -0300

    fix the menu jscript - closing is not working

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const menuBtn = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
+const clsModal = document.querySelector('#clsModal');
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   mobileMenu.classList.toggle("active");
 })
 
+document.querySelectorAll('nav-links').forEach(m => m.addEventListener('click', () => {
+  clsModal.classList.remove("active");
+}));
 
 
-/*const seeProjectBtn = document.querySelectorAll('.see-project');
+/*
+const seeProjectBtn = document.querySelectorAll('.see-project');
 // const clsMenuBtn = document.querySelectorAll('');
 const detailsModal = document.querySelectorAll('.card1 .details');
 const body = document.body;
@@ -22,5 +27,4 @@ seeProjectBtn.addEventListener('click',function() {
 //   mobMenu.classList.remove('display-block');
 //   body.classList.remove('modal-on')
 // });
-
-
+*/
