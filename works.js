ad33fbbdb7fffe38a6fe458318286404a5664f0d
Tiago Marin <tiago.lelinski@gmail.com>
2022-06-21 18:11:33 -0300

    add featured card with javascript

--- works.js
+++ works.js
@@ -1,9 +1,21 @@
-function cardsSection (teachers)
-  teachers = {
+const recentWorks = document.querySelector('#recent-works');
+
+// function cardsSection (portifolio)
+const portifolio = {
+    featured: {
+        heading: "title of Featured card",
+        highlight: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+        image: "assets/project-featured.jpg",
+        badge1: "css",
+        badge2: "html",
+        badge3: "Bootstrap",
+        badge4: "Ruby",
+        button: "See project",
+    },    
     card1: {
         heading: "title of card 1",
         highlight: "A daily selection of privately personalized reads, no accounts or sign-ups require has been the industrys standard",
-        image: "assets\project2.jpg",
+        image: "assets/project1-web.jpg",
         badge1: "css",
         badge2: "html",
         badge3: "Bootstrap",
@@ -11,7 +23,7 @@ function cardsSection (teachers)
         button: "See project",
     },
     card2: {
-        heading: "title of card 1",
+        heading: "title of card 2",
         highlight: "A daily selection of privately personalized reads, no accounts or sign-ups require has been the industrys standard",
         image: "assets\project2.jpg",
         badge1: "css",
@@ -21,9 +33,9 @@ function cardsSection (teachers)
         button: "See project",
     },
     card3: {
-        heading: "title of card 1",
+        heading: "title of card 3",
         highlight: "A daily selection of privately personalized reads, no accounts or sign-ups require has been the industrys standard",
-        image: "assets\project2.jpg",
+        image: "assets\project3.jpg",
         badge1: "css",
         badge2: "html",
         badge3: "Bootstrap",
@@ -31,9 +43,9 @@ function cardsSection (teachers)
         button: "See project",
     },
     card4: {
-        heading: "title of card 1",
+        heading: "title of card 4",
         highlight: "A daily selection of privately personalized reads, no accounts or sign-ups require has been the industrys standard",
-        image: "assets\project2.jpg",
+        image: "assets\project4.jpg",
         badge1: "css",
         badge2: "html",
         badge3: "Bootstrap",
@@ -41,9 +53,9 @@ function cardsSection (teachers)
         button: "See project",
     }, 
     card5: {
-        heading: "title of card 1",
+        heading: "title of card 5",
         highlight: "A daily selection of privately personalized reads, no accounts or sign-ups require has been the industrys standard",
-        image: "assets\project2.jpg",
+        image: "assets\project5.jpg",
         badge1: "css",
         badge2: "html",
         badge3: "Bootstrap",
@@ -51,9 +63,9 @@ function cardsSection (teachers)
         button: "See project",
     }, 
     card6: {
-        heading: "title of card 1",
+        heading: "title of card 6",
         highlight: "A daily selection of privately personalized reads, no accounts or sign-ups require has been the industrys standard",
-        image: "assets\project2.jpg",
+        image: "assets\project6.jpg",
         badge1: "css",
         badge2: "html",
         badge3: "Bootstrap",
@@ -61,7 +73,54 @@ function cardsSection (teachers)
         button: "See project",
     }
 }
-for (let card1 in arr) {
+// create featured card
+let article = document.createElement('article');
+article.classList = 'featured';
+recentWorks.appendChild(article);
+
+//create img of card
+let img = document.createElement('img');
+img.src = (portifolio.featured.image);
+article.appendChild(img);
+
+//create card wrap
+let divWrap = document.createElement('div');
+divWrap.classList = 'wrap-featured';
+article.appendChild(divWrap);
+
+// create title of card
+let h3 = document.createElement('h3');
+h3.classList = 'card-h';
+h3.innerHTML = portifolio.featured.heading;
+divWrap.appendChild(h3);
+
+// create card text
+let p = document.createElement('p');
+p.id = 'featured-p';
+p.innerHTML = portifolio.featured.highlight;
+divWrap.appendChild(p);
+
+//create the div of badges
+div = document.createElement('div');
+div.classList = 'lang';
+divWrap.appendChild(div);
+
+// add ul of badges
+let ul = document.createElement('ul');
+div.appendChild(ul);
+
+//create badge items
+//create a for loop to create 3 of them
+let li = document.createElement('li')
+li.classList ='lang-icons';
+ul.appendChild(li);
+let button = document.createElement('button');
+button.innerHTML = portifolio.featured.badge1;
+li.appendChild(button);
 
 
-}
+//create the button see project
+button = document.createElement('button');
+button.classList = 'small-button see-project';
+button.innerHTML = 'See Project';
+divWrap.appendChild(button);
