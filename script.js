d3d9bae321fec8c18d7c574fbef17c950727e906
Tiago Marin <tiago.lelinski@gmail.com>
2022-06-22 19:56:23 -0300

    fix details pop up style

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const menuBtn = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const clsmodal = document.querySelector('#cls-modal');
 
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("active");
-  mobileMenu.classList.toggle("active");
-})
-
-clsmodal.addEventListener("click", () => {
-  mobileMenu.classList.remove("active")
-  menuBtn.classList.remove("active");
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
 });
 
+clsmodal.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+  menuBtn.classList.remove('active');
+});
