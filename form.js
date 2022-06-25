57bc8e4c2f6ae0c261008cf3536066fc8a4f8d0b
Tiago Marin <tiago.lelinski@gmail.com>
2022-06-25 13:00:28 -0300

    add local storage of the form

--- form.js
+++ form.js
@@ -13,3 +13,28 @@ document.querySelector('#submit').addEventListener('click', (event) => {
     5000);
   }
 });
+
+// local storage path
+const nameForm = document.querySelector('#name');
+const emailForm = document.querySelector('#email');
+const messageForm = document.querySelector('#message');
+function handleChange() {
+  const formData = {
+    fullName: nameForm.value,
+    email: emailForm.value,
+    message: messageForm.value,
+  };
+  localStorage.setItem('form', JSON.stringify(formData));
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const getFormValue = localStorage.getItem('form');
+  if (getFormValue) {
+    const formObject = JSON.parse(getFormValue);
+    nameForm.value = formObject.fullName;
+    emailForm.value = formObject.email;
+    messageForm.value = formObject.message;
+  }
+});
+nameForm.onchange = handleChange;
+emailForm.onchange = handleChange;
+messageForm.onchange = handleChange;
